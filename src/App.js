0a1e6434553cc863209e233b401b8139d29891af
halimAremu <halimat3000@gmail.com>
2023-06-22 13:00:17 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import AddTask from './components/AddTask.js';
+import TaskButton from './components/TaskButton.js';
+import {useState}  from 'react';
 
 const TASKS = [
   {
@@ -13,16 +16,51 @@ const TASKS = [
     title: 'Cook Pasta',
     isComplete: true,
   },
+  {
+    id: 3,
+    title: 'Do Laundry',
+    isComplete: true,
+  },
+  {
+    id: 4,
+    title: 'Go Shopping',
+    isComplete: false,
+  },
+  {
+    id: 5,
+    title: 'Clean House',
+    isComplete: false,
+  },
+  {
+    id: 6,
+    title: 'Walk Dog',
+    isComplete: true,
+  },
 ];
 
 const App = () => {
+
+  const [taskData, setTaskData]= useState(TASKS);
+
+  const onComplete = (id) => {
+    const completed = taskData.map((task)=>{
+      if(task.id === id){
+        return {...task, isComplete: !task.isComplete};
+      } else {
+        return task;
+      }
+    });
+    setTaskData(completed);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div><TaskList tasks={taskData} onComplete = {onComplete} /></div>
+        <div><AddTask/></div>
+        <div><TaskButton/></div>
       </main>
     </div>
   );
